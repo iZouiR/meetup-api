31f4eaf9007c2afb731d45dd901e60deeedb3fde
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-17 13:45:07 +0300

    create sessionFactory bean

--- src/main/java/self/izouir/modsentesttask/ModsenTestTaskApplication.java
+++ src/main/java/self/izouir/modsentesttask/ModsenTestTaskApplication.java
@@ -2,12 +2,24 @@ package self.izouir.modsentesttask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ModsenTestTaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ModsenTestTaskApplication.class, args);
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource);
+        localSessionFactoryBean.setPackagesToScan("self.izouir.modsentesttask.entities");
+        return localSessionFactoryBean;
     }
 
 }

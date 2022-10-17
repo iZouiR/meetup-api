aeb37901542da99bc18cb4e538ca8f22ec88fab9
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-17 20:03:01 +0300

    update MeetServiceImpl: findAll() method return type is List<MeetDto>, add update method, change Meet data type to MeetDto, add exception-throwing logic

--- src/main/java/self/izouir/modsentesttask/service/impl/MeetServiceImpl.java
+++ src/main/java/self/izouir/modsentesttask/service/impl/MeetServiceImpl.java
@@ -3,33 +3,52 @@ package self.izouir.modsentesttask.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import self.izouir.modsentesttask.dao.MeetDao;
+import self.izouir.modsentesttask.dto.MeetDto;
 import self.izouir.modsentesttask.entity.Meet;
+import self.izouir.modsentesttask.exception.MeetExistsException;
+import self.izouir.modsentesttask.exception.MeetNotFoundException;
+import self.izouir.modsentesttask.mapper.MeetMapper;
 import self.izouir.modsentesttask.service.MeetService;
 
-import java.util.Set;
+import java.util.List;
 
 @Service
 public class MeetServiceImpl implements MeetService {
     private final MeetDao meetDao;
+    private final MeetMapper meetMapper;
 
     @Autowired
-    public MeetServiceImpl(final MeetDao meetDao) {
+    public MeetServiceImpl(final MeetDao meetDao,
+                           final MeetMapper meetMapper) {
         this.meetDao = meetDao;
+        this.meetMapper = meetMapper;
     }
 
     @Override
-    public Set<Meet> findAll() {
-        return meetDao.findAll();
+    public List<MeetDto> findAll() {
+        return meetMapper.mapToDtos(meetDao.findAll());
     }
 
     @Override
-    public Meet find(final Long meetId) {
-        return meetDao.find(meetId);
+    public MeetDto find(final Long meetId) {
+        final Meet meet = meetDao.find(meetId).orElseThrow(
+                () -> new MeetNotFoundException("Meet with meetId = " + meetId + " was not found"));
+        return meetMapper.mapToDto(meet);
     }
 
     @Override
-    public void save(final Meet meet) {
-        meetDao.save(meet);
+    public void save(final MeetDto meetDto) {
+        if (meetDao.find(meetDto.getMeetId()).isPresent()) {
+            throw new MeetExistsException("Meet with meetId = " + meetDto.getMeetId() + " already exists");
+        }
+        meetDao.save(meetMapper.mapToEntity(meetDto));
+    }
+
+    @Override
+    public void update(final MeetDto meetDto) {
+        meetDao.find(meetDto.getMeetId()).orElseThrow(
+                () -> new MeetNotFoundException("Meet with meetId = " + meetDto.getMeetId() + " was not found"));
+        meetDao.update(meetMapper.mapToEntity(meetDto));
     }
 
     @Override

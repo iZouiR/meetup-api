3cf6f0124b997b40d54fde2d854f2f9d4943b657
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-18 01:53:13 +0300

    rename dto package to repository, update MeetRepository: add filter method, override it in MeetRepositoryImpl class

--- src/main/java/self/izouir/modsentesttask/repository/impl/MeetRepositoryImpl.java
+++ src/main/java/self/izouir/modsentesttask/repository/impl/MeetRepositoryImpl.java
@@ -1,23 +1,24 @@
-package self.izouir.modsentesttask.dao.impl;
+package self.izouir.modsentesttask.repository.impl;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import self.izouir.modsentesttask.dao.MeetDao;
 import self.izouir.modsentesttask.entity.Meet;
+import self.izouir.modsentesttask.repository.MeetRepository;
 
 import javax.persistence.Query;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class MeetDaoImpl implements MeetDao {
+public class MeetRepositoryImpl implements MeetRepository {
     private final SessionFactory sessionFactory;
 
     @Autowired
-    public MeetDaoImpl(final SessionFactory sessionFactory) {
+    public MeetRepositoryImpl(final SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
@@ -51,8 +52,18 @@ public class MeetDaoImpl implements MeetDao {
     @Transactional
     public void delete(final Long meetId) {
         final Session session = sessionFactory.getCurrentSession();
-        final Query query = session.createQuery("delete from Meet where meetId =:meetId");
+        final Query query = session.createQuery("delete from Meet where meetId = :meetId");
         query.setParameter("meetId", meetId);
         query.executeUpdate();
+    }
+
+    @Override
+    public List<Meet> findAllFilter(final String title, final String keeper, final Timestamp date) {
+        final Session session = sessionFactory.getCurrentSession();
+        final Query query = session.createQuery("from Meet where lower(title) like :title and lower(keeper) like :keeper and date >= :date", Meet.class);
+        query.setParameter("title", "%" + title.toLowerCase() + "%");
+        query.setParameter("keeper", "%" + keeper.toLowerCase() + "%");
+        query.setParameter("date", date);
+        return query.getResultList();
     }
 }

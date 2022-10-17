a212a93c10fafeacbb703b5efb31654dccd543eb
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-17 10:35:23 +0300

    init commit

---
+++ src/main/java/self/izouir/modsentesttask/ModsenTestTaskApplication.java
@@ -0,0 +1,13 @@
+package self.izouir.modsentesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModsenTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/self/izouir/modsentesttask/ModsenTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package self.izouir.modsentesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsenTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

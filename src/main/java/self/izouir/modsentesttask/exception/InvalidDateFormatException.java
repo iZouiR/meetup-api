1c051380a7ef544510c20df8a1feeb76316bff98
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-18 16:58:01 +0300

    add exception package: add InvalidDateFormatException class

---
+++ src/main/java/self/izouir/modsentesttask/exception/InvalidDateFormatException.java
@@ -0,0 +1,7 @@
+package self.izouir.modsentesttask.exception;
+
+public class InvalidDateFormatException extends IllegalArgumentException {
+    public InvalidDateFormatException(final String message) {
+        super(message);
+    }
+}

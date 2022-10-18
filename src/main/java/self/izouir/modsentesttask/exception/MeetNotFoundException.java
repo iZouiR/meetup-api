26f6f53382077e8e93a0d8a230695d19262ea3c5
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-18 13:28:39 +0300

    remove redundant converter and exception packages

--- src/main/java/self/izouir/modsentesttask/exception/MeetNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package self.izouir.modsentesttask.exception;
-
-import javax.persistence.EntityNotFoundException;
-
-public class MeetNotFoundException extends EntityNotFoundException {
-    public MeetNotFoundException(final String message) {
-        super(message);
-    }
-}

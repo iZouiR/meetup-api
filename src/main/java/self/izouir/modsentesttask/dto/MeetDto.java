2eae2e0d11f6e845f7aa43bb23dc6d461cc8d84e
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-18 13:18:50 +0300

    update MeetDto: add validation annotations

--- src/main/java/self/izouir/modsentesttask/dto/MeetDto.java
+++ src/main/java/self/izouir/modsentesttask/dto/MeetDto.java
@@ -2,7 +2,20 @@ package self.izouir.modsentesttask.dto;
 
 import self.izouir.modsentesttask.entity.Place;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
-public record MeetDto(Long meetId, String title, String description, String keeper, Timestamp date, Place place) {
+public record MeetDto(Long meetId,
+                      @NotNull
+                      @NotBlank
+                      String title,
+                      String description,
+                      @NotNull
+                      @NotBlank
+                      String keeper,
+                      @NotNull
+                      Timestamp date,
+                      @NotNull
+                      Place place) {
 }

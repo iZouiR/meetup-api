1580c22f4b8731ffccc075d3f9f42f93ec72fe4d
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-20 16:53:46 +0300

    config OpenAPI

--- src/main/java/self/izouir/modsentesttask/ModsenTestTaskApplication.java
+++ src/main/java/self/izouir/modsentesttask/ModsenTestTaskApplication.java
@@ -1,9 +1,12 @@
 package self.izouir.modsentesttask;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Meetup API", version = "1.0", description = "Modsen Test Task API"))
 public class ModsenTestTaskApplication {
 
     public static void main(final String[] args) {

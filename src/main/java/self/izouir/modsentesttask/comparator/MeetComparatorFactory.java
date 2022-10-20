2ae31970905422de36e79700ead22784aeb945d7
Timofey Kozlov <iZouiR@jotaro.ru>
2022-10-20 14:53:06 +0300

    change MeetComparatorFactory type to class for mocking in tests

--- src/main/java/self/izouir/modsentesttask/comparator/MeetComparatorFactory.java
+++ src/main/java/self/izouir/modsentesttask/comparator/MeetComparatorFactory.java
@@ -6,10 +6,22 @@ import self.izouir.modsentesttask.entity.Meet;
 import java.util.Comparator;
 
 @Component
-public record MeetComparatorFactory(MeetComparatorByMeetId byMeetId,
-                                    MeetComparatorByTitle byTitle,
-                                    MeetComparatorByKeeper byKeeper,
-                                    MeetComparatorByDate byDate) {
+public class MeetComparatorFactory {
+    private final MeetComparatorByMeetId byMeetId;
+    private final MeetComparatorByTitle byTitle;
+    private final MeetComparatorByKeeper byKeeper;
+    private final MeetComparatorByDate byDate;
+
+    public MeetComparatorFactory(final MeetComparatorByMeetId byMeetId,
+                                 final MeetComparatorByTitle byTitle,
+                                 final MeetComparatorByKeeper byKeeper,
+                                 final MeetComparatorByDate byDate) {
+        this.byMeetId = byMeetId;
+        this.byTitle = byTitle;
+        this.byKeeper = byKeeper;
+        this.byDate = byDate;
+    }
+
     public Comparator<Meet> get(final String comparator) {
         switch (comparator) {
             case "title" -> {
